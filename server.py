6b56deaf26e66411b0699632d42a50d0f0a4d5bf
NoahOConnor44 <46578340+NoahOConnor44@users.noreply.github.com>
2022-04-19 21:02:30 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,32 @@
-#Code for the server
+import threading
+import socket
+
+port = 5000
+host = "0.0.0.0"
+
+server = socket.socket()
+
+server.bind((host,port))
+server.listen(5)
+
+clients = []
+
+def start():
+    while(True):
+        conn, addr = server.accept()
+        clients.append(conn)
+        clientThread = threading.Thread(target = send, args = (conn, ))
+        clientThread.start()
+
+
+def send(incomingConnection):
+    try:
+        while(True):
+            data = incomingConnection.recv(4096)
+            for client in clients:
+                if client != incomingConnection:
+                    client.send(data)
+    except:
+        print("Client disconnected")
+
+start()
